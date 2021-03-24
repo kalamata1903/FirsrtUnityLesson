55df7c0dc51a0e518c56736a6e00408e9b5ca0bf
kalamata1903 <mehmet1903ozyurek@gmail.com>
2021-03-24 15:13:45 +0300

    FirstScript

---
+++ Assets/Script/FirstLessonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstLessonScript : MonoBehaviour
+{
+    public int tamsayi = 16031995;
+    public int tamsayi2 = 17051993;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
